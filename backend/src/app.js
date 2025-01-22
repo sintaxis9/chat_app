62d629e7409036af7fc8fe46647fcd7f7833fe3f
sintaxis9 <skizosyntax@duck.com>
2025-01-22 17:48:55 -0300

    heh

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,12 @@ import morgan from "morgan";
 
 const app = express();
 
-// Middlewares
 app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando correctamente");
+  res.send("server working");
 });
 
 export default app;
