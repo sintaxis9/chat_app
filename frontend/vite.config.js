4241fee6fcc443b7554de5c98d3a23f65a9cf484
sintaxis9 <skizosyntax@duck.com>
2025-01-29 18:41:38 -0300

    backend/frontend | docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/socket.io": {
-        target: "http://localhost:3000",
-        ws: true,
-      },
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
     },
   },
 });
